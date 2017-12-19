dba47b8bcb385cc040a74a90fb75f32c9278f0e1
Hank Tom <hanktom@gmail.com>
2017-12-19 11:32:55 +0800

    ages resources array Spinner

--- app/src/main/java/com/tom/atm/UserInfoActivity.java
+++ app/src/main/java/com/tom/atm/UserInfoActivity.java
@@ -2,18 +2,37 @@ package com.tom.atm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class UserInfoActivity extends AppCompatActivity {
+
+    private static final String TAG = UserInfoActivity.class.getSimpleName();
+    private Spinner ages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        ages = (Spinner) findViewById(R.id.ages);
+//        ArrayList<String> data = new ArrayList<>();
+//        for (int i = 15; i<=40; i++){
+//            data.add(i+"");
+//        }
+//        String[] data = getResources().getStringArray(R.array.ages);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.aaa, android.R.layout.simple_list_item_1);
+        ages.setAdapter(adapter);
     }
 
     public void ok(View view){
+        Log.d(TAG, "ok: " + ages.getSelectedItem().toString());
+        int age = Integer.parseInt(ages.getSelectedItem().toString());
         EditText edNickname = (EditText) findViewById(R.id.user_nickname);
         EditText edPhone = (EditText) findViewById(R.id.user_phone);
         String nickname = edNickname.getText().toString();
